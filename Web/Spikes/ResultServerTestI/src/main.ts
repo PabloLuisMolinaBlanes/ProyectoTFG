44668105d499c6dba90a71e4fc2df7d124cb3e3a
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-03-29 14:29:09 +0200

    Added a basic example web server and some updates

---
+++ Web/Spikes/ResultServerTestI/src/main.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import { AxiosResponse } from 'axios'
+
+const app = express()
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}
+const axios = require("axios")
+const port = 3000
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.post("/upload", async (req,res) => {
+    axios.post("http://localhost:5000/", req.body).then((response : AxiosResponse) => {
+        res.setHeader('content-type', 'text/plain')
+        res.send(response.data)
+    })
+});
+
+app.listen(port, () => {
+    console.log(`ResultServer Test backend server is now listening in port ${port}`)
+})
