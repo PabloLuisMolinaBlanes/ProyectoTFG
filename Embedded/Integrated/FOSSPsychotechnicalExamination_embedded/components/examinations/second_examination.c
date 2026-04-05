105ee648df28119f3980357f0285d4bd84b37ddd
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-05 21:19:57 +0200

    Extendida implementacion de second_examination

--- Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/components/examinations/second_examination.c
+++ Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/components/examinations/second_examination.c
@@ -13,6 +13,7 @@ adc_oneshot_unit_handle_t handle = NULL;
 adc_oneshot_unit_handle_t handle_2 = NULL;
 adc_cali_handle_t cali_handle = NULL;
 adc_cali_handle_t cali_handle_2 = NULL;
+int configured = 0;
 
 /* Configuraciones */
 adc_oneshot_unit_init_cfg_t init_config1 = {
@@ -43,15 +44,26 @@ adc_cali_line_fitting_config_t cali_config_2 = {
     };
 
 void configure_second_test() {
+    if (configured == 1) {
+        return;
+    } 
     
+    /* Configuración de unidades ADC */
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &handle));
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &handle_2));
     
+    /* Configuración de canales */
     ESP_ERROR_CHECK(adc_oneshot_config_channel(handle, ADC_CHANNEL_7, &config));
     ESP_ERROR_CHECK(adc_oneshot_config_channel(handle_2, ADC_CHANNEL_7, &config_2));
     
+    /* Configuración de calibracion */
     ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_config, &cali_handle));
     ESP_ERROR_CHECK(adc_cali_create_scheme_line_fitting(&cali_config_2, &cali_handle_2));
+
+    if (is_uart_already_configured() == 0) {
+        configure_uart_with_default_options();
+    }
+    configured = 1;
 }
 
 void second_examination_loop() {
