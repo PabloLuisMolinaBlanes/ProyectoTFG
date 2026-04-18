6a8f13ff99170d0ee8d57245907e4da5a3bf4d79
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-18 20:49:53 +0200

    Reconfigurado prisma y el proyecto

--- Cloud/Spikes/SimpleCRBackend/src/server.ts
+++ Cloud/Spikes/SimpleCRBackend/src/server.ts
@@ -2,8 +2,17 @@
 import express from 'express'
 import cors from 'cors'
 import { PrismaClient, profile, test} from "./generated/prisma";
-const prisma : PrismaClient = new PrismaClient()
-import { AxiosResponse } from 'axios'
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+/*Database setup*/
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+const prisma : PrismaClient = new PrismaClient({ adapter });
 
 /*ORM functions*/
 
@@ -14,7 +23,7 @@ async function createTest(id_received: string, hospital_password: string, first_
         } 
     })
     if (hospital_received == undefined) {
-        return "Invalid password";
+        return {error: "Invalid password"};
     } 
     const test = await prisma.test.create({
         data: {
@@ -22,7 +31,7 @@ async function createTest(id_received: string, hospital_password: string, first_
             results_reaction_time: first_exam,
             results_first_potentiometer: second_exam_first_potentio,
             results_second_potentiometer: second_exam_second_potentio,
-            hospital_id: hospital_received?.hospital_id
+            hospital_id: hospital_received.hospital_id
         }
     })
     return test;
@@ -35,7 +44,7 @@ async function getAllTestsPerHospital(hospital_password: string) {
         } 
     })
     if (hospital_received == undefined) {
-        return("Invalid password");
+        return {error: "Invalid password"};
     } 
     const tests = await prisma.test.findMany({
         where: {
@@ -51,7 +60,6 @@ const corsOptions = {
     origin: 'http://localhost:5173',
     optionsSuccessStatus: 200
 }
-const axios = require("axios")
 const port = 3000
 
 app.use(cors(corsOptions))
