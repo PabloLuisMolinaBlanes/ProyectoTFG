9eb7e91227524e499baa82a34a8c66a1efb75dca
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-25 19:11:06 +0200

    Agregados comentarios a main c de embedded

--- Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/main/main.c
+++ Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/main/main.c
@@ -1,15 +1,21 @@
+/*Includes*/
+
 #include <stdio.h>
 #include "first_examination.h"
 #include "second_examination.h"
 #include "uart.h"
 
+/*Definiciones*/
 #define MILLISECONDS 20
 #define TEST_1 1
 #define TEST_2 2
 
+/*Método principal*/
 void app_main(void)
 {
+    // Configura el dispositivo UART
     configure_uart_with_default_options();
+    /*Bucle principal. Lee del dispositivo UART cada 200 milisegundos, y actua conforme a lo recibido.*/
     while (1) {
         int result = read_from_uart(200);
         switch(result){
