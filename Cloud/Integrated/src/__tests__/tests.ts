e66f6f8c278f92a37d1e2b84b5418b9203f5c361
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-27 20:52:08 +0200

    Agregado otro test

--- Cloud/Integrated/src/__tests__/tests.ts
+++ Cloud/Integrated/src/__tests__/tests.ts
@@ -1,6 +1,13 @@
 import {describe, expect, test} from '@jest/globals';
-import { comparePasswords } from '../password'; 
+import { comparePasswords, hashPassword } from '../password'; 
 
-test('Password should equal its hash', async () => {
+test('Cleartext password should compare to its hash', async () => {
   expect(await comparePasswords("test", "$2a$10$LSxhFhbPYofMQORtPRAws.SrzcmtWMBQjVxctuNLu3bsIQDZdE9oe")).toBe(true);
+});
+
+test('Cleartext password should compare to itself hash', async () => {
+  // Arrange
+  const passwordHash = await hashPassword("test");
+  // Assert
+  expect(await comparePasswords("test", passwordHash)).toBe(true);
 });
