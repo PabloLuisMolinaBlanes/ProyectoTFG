b7e1713fe728bc7f169959abfdf7bceec3eb5374
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-28 18:00:57 +0200

    Arreglados problemas con header de segunda examinacion y agregados multiples nuevos tests

--- SystemSoftware/Integrated/src/examinations/include/second_examination/second_examination.hpp
+++ SystemSoftware/Integrated/src/examinations/include/second_examination/second_examination.hpp
@@ -1,13 +1,34 @@
 #include <SFML/Graphics.hpp>
 
-struct Point;
-
-class Path;
-
-class Car;
+class Car {
+    public:
+        // Verdadera posición x del coche.
+        float actual_x;
+        // Verdadera posición y del coche
+        float actual_y;
+        // Posición en x mostrada, no puede superar los bordes en x de la pantalla
+        float x;
+        // Posición en y mostrada, no puede superar los bordes en y de la pantalla
+        float y;
+        sf::RectangleShape shape;
+        //Realiza el movimiento del coche en pantalla a partir del desplazamiento dx
+        void move(float x_received);
+};
 
 sf::RectangleShape initShape(int pos_x, int pos_y);
 
 sf::RectangleShape initShape(int pos_x, int pos_y, float size_x, float size_y);
 
 Car initCar(int car_x, int car_y);
+
+void initializeWalls(sf::RectangleShape * walls, int initial_x_wall, float size_x, float size_y);
+
+void setPositions(sf::RectangleShape * walls, int * positions, int counter, int direction);
+
+void initializePositions(int * positions);
+
+int verifyCollision(Car car, sf::RectangleShape wall);
+
+int resolvePosition(int index, int * collection, int size_array);
+
+void initializeLookUpTable(int * collection, int multiple);
--- SystemSoftware/Integrated/test/src/second_examination_test.cpp
+++ SystemSoftware/Integrated/test/src/second_examination_test.cpp
@@ -26,3 +26,63 @@ TEST(InitShapeInitsShapeWithCorrectPosXAndPosYAndSize, BasicAssertions) {
     bool finalResult = result && result_2;
     EXPECT_EQ(true, finalResult);
 }
+
+TEST(InitCarActuallyInitsTheCar, BasicAssertions) {
+    // Arrange
+    Car car = initCar(200, 200);
+    // Assert
+    bool result = car.x == 200;
+    bool result_2 = car.y == 200;
+    bool result_3 = car.actual_x == 200;
+    bool result_4 = car.actual_y == 200;
+    EXPECT_EQ(true, result && result_2 && result_3 && result_4);
+}
+
+TEST(InitializeWallsInitializesWallsCorectly, BasicAssertions) {
+    sf::RectangleShape walls[5];
+    int initial_x_wall = 0;
+    float size_x = 200;
+    float size_y = 200; 
+    // Arrange
+    initializeWalls(walls, initial_x_wall, size_x, size_y);
+    // Assert
+    bool result = true;
+    for (int i = 0; i<5; i++) {
+        result = result & walls[i].getPosition().x == initial_x_wall;
+        result = result & walls[i].getSize().x == size_x;
+        result = result & walls[i].getSize().y == size_y;
+    } 
+    EXPECT_EQ(true, result);
+}
+
+TEST(InitializePositionsInitializesPositionsCorectly, BasicAssertions) {
+    int positions[200];
+    // Arrange
+    initializePositions(positions);
+    // Assert
+    bool result = true;
+    for (int i = 0; i<200; i++) {
+        result = result && (positions[i] == -10 || positions[i] == 0 || positions[i] == 10);
+    } 
+    EXPECT_EQ(true, result);
+}
+
+TEST(InitializeLookUpTableInitializesTableCorectly, BasicAssertions) {
+    int collection[3000];
+    int multiple = 5;
+    // Arrange
+    initializeLookUpTable(collection, multiple);
+    // Assert
+    bool result = true;
+    int past_past_collection = -1;
+    for (int i = 0; i<3000; i += multiple) {
+        int past_collection = collection[i];
+        result = result && collection[i] != past_past_collection;  
+        for (int j = i; j < i+multiple; j++) {
+            result = result && collection[j] == past_collection;
+            past_collection = collection[j];  
+        }
+        past_past_collection = collection[i]; 
+    }
+    EXPECT_EQ(true, result);
+}
