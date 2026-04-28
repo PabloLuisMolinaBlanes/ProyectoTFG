e8c0bd52e85543cfa93622cb613824f6af9057f5
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-04-28 16:56:16 +0200

    Agregadas pruebas para casi todos los metodos de first_examination.cpp

--- SystemSoftware/Integrated/src/examinations/include/first_examination/first_examination.hpp
+++ SystemSoftware/Integrated/src/examinations/include/first_examination/first_examination.hpp
@@ -1,10 +1,40 @@
 #include <SFML/Graphics.hpp>
 
-struct Point;
+struct Point {
+    float x;
+    float y;
+};
 
-class Path;
+class Path {
+    public:
+        // Punto inicial del camino
+        struct Point P0;
+        // Punto final del camino
+        struct Point P1;
+        //Genera un camino a seguir para el coche
+        void generatePath(float car_x, float car_y, float screen_x);
+};
 
-class Car;
+class Car {
+    public:
+        // Verdadera posición x del coche.
+        float actual_x;
+        // Verdadera posición y del coche
+        float actual_y;
+        // Posición en x mostrada, no puede superar los bordes en x de la pantalla
+        float x;
+        // Posición en y mostrada, no puede superar los bordes en y de la pantalla
+        float y;
+        // Tiempo pasado (se mide en intervalos de velocity, no utilizamos segundos reales)
+        float t;
+        // Intervalos de tiempo (1 por ejecución)
+        float velocity;
+        sf::RectangleShape shape;
+        //Calcula el valor de la ecuación X_F = X_0+V*t, y utiliza este valor para calcular el desplazamiento en X que se ha de hacer desde la posición actual del coche
+        void moveAlongPath(struct Path *path, float screen_x);
+        //Realiza el movimiento del coche en pantalla a partir del desplazamiento dx
+        void move(float screen_x, float dx);
+};
 
 sf::RectangleShape initShape(int pos_x, int pos_y);
 
--- SystemSoftware/Integrated/test/src/first_examination_test.cpp
+++ SystemSoftware/Integrated/test/src/first_examination_test.cpp
@@ -26,3 +26,30 @@ TEST(InitShapeInitsShapeWithCorrectPosXAndPosYAndSize, BasicAssertions) {
     bool finalResult = result && result_2;
     EXPECT_EQ(true, finalResult);
 }
+
+TEST(InitCarActuallyInitsTheCar, BasicAssertions) {
+    // Arrange
+    Car car = initCar(200, 200);
+    // Assert
+    bool result = car.x == 200;
+    bool result_2 = car.y == 200;
+    bool result_3 = car.actual_x == 200;
+    bool result_4 = car.actual_y == 200;
+    EXPECT_EQ(true, result && result_2 && result_3 && result_4);
+}
+
+TEST(ResetTestActuallyAffectsTheCar, BasicAssertions) {
+    // Arrange
+    Car car = initCar(200, 200);
+    float car_y_old = car.y;
+    float car_actual_y_old = car.actual_y;
+    // Act
+    reset_test(&car);
+    // Assert
+    bool result = car.t == 0;
+    bool result_2 = car.x == 300.0f;
+    bool result_3 = car.actual_x == 300.0f;
+    bool result_4 = car.y == car_y_old;
+    bool result_5 = car.actual_y == car_actual_y_old;
+    EXPECT_EQ(true, result && result_2 && result_3 && result_4 && result_5);
+}
