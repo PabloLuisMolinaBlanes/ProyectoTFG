a63a21b7c2e69e3d9e34fd42e7f3dad23a5fd31b
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-05-17 17:25:56 +0200

    Arreglado script de instalacion para sistemas empotrados

--- Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/install.sh
+++ Embedded/Integrated/FOSSPsychotechnicalExamination_embedded/install.sh
@@ -1,2 +1,4 @@
-# Execute with source "$HOME/.espressif/tools/activate_idf_v5.5.3.sh"
-idf.py -p /dev/ttyUSB0 build
+#!/bin/bash
+VERSION=v5.5.3
+IDF_TOOLS_PATH=$HOME/.espressif/$VERSION/esp-idf/tools
+bash -c "source '$HOME/.espressif/tools/activate_idf_$VERSION.sh';$IDF_TOOLS_PATH/idf.py -p /dev/ttyUSB0 build"
