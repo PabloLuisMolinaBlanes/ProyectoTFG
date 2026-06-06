f3f9791338fea99e0d428473d6a72e51cd7e84ee
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-06-06 18:42:14 +0200

    Migrado proyecto web a tests con vitest

--- Web/Integrated/src/__tests__/identificacion.test.ts
+++ Web/Integrated/src/__tests__/identificacion.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, test} from '@jest/globals';
+import {expect, test} from 'vitest';
 import { generateRandomString, verifyNIFLetter } from '../utils';
 
 test('Generated password should always have parameter length length', async () => {
