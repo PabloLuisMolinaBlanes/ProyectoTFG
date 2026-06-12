d1a6f087d1be3a0de656cc6ca02257c70cbc537a
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2026-06-12 15:16:55 +0200

    Intentado arreglo otra vez

--- Cloud/Integrated/db/schema.sql
+++ Cloud/Integrated/db/schema.sql
@@ -58,7 +58,7 @@ DROP TABLE IF EXISTS `test`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `test` (
-  `id` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` varchar(2001) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `hospital_id` int NOT NULL,
   `results_reaction_time` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `results_first_potentiometer` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
